7730cbe364f0d7f64c288b7efabcb5d941b382f1
Isaac Colls <colls_isaac@hotmail.com>
2019-04-08 14:14:56 -0400

    working on field entry for SignUp

--- Componentes/NoAutenticados/Formas/SignUpForm.js
+++ Componentes/NoAutenticados/Formas/SignUpForm.js
@@ -3,27 +3,42 @@ import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { Field, reduxForm } from 'redux-form';
 
 const fieldNombre = (props) => {
-    // console.log(props); //props.input.onChange <= es la q inyecta fields, no del TextInput
+    console.log(props); //props.input.onChange <= es la q inyecta fields, no del TextInput
     return (
         <TextInput
-            placeholder="Texto desde fieldNombre"
+            placeholder={props.ph}
             onChangeText={props.input.onChange}
             value={props.input.value}
+            keyboardType={props.input.name === 'correo' ? 'email-address' : 'default'}
+            autoCapitalize='none'
+            secureTextEntry={!!(props.input.name === 'password' || props.input.name === 'confirmacion')}
         />
     );
 };
 
 const SignUpForm = (props) => {
-    console.log(props);
+    // console.log(props);
     return(
         <View>
             <Field
                 name="nombre"
                 component={fieldNombre}
+                ph="nombre"
             />
             <Field
                 name="correo"
                 component={fieldNombre}
+                ph="correo@correo.com"
+            />
+            <Field
+                name="password"
+                component={fieldNombre}
+                ph="*******"
+            />
+            <Field
+                name="confirmacion"
+                component={fieldNombre}
+                ph="*******"
             />
             <Text>Redux Form</Text>
             <Button
