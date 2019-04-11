fabf5be938a7c21ae8e6e7198144c0aa92ad935c
Isaac Colls <colls_isaac@hotmail.com>
2019-04-10 22:53:12 -0400

    Firebase configuration

---
+++ Store/Servicios/Firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyAaJ2TuBq1jD3zDXs9-euQGVUFpJs8GaPc",
+    authDomain: "instagram-clone-421.firebaseapp.com",
+    databaseURL: "https://instagram-clone-421.firebaseio.com",
+    projectId: "instagram-clone-421",
+    storageBucket: "instagram-clone-421.appspot.com",
+    messagingSenderId: "991604369157"
+};
+firebase.initializeApp(config);
